b151ceb040e6a0df975776b0c120940ee50d44a3
mrGoonies <gooniesdev@gmail.com>
2025-08-26 06:33:48 -0400

    Solucionando Bug del contador de palabras

--- index.js
+++ index.js
@@ -4,31 +4,32 @@ const characterNumberEl = document.querySelector(".stat__number--characters");
 const twitterNumberEl = document.querySelector(".stat__number--twitter");
 const facebookNumberEl = document.querySelector(".stat__number--facebook");
 
+textareaEl.addEventListener("input", () => {
+  // Obtener largo de string de lo ingresado en el textarea
+  const lengthOfCharacters = textareaEl.value.length;
 
-
-textareaEl.addEventListener('input', () => {
-    // Obtener largo de string de lo ingresado en el textarea
-    const lengthOfCharacters = textareaEl.value.length;
-    
-    // Asignar valores 
-    const wordCount = textareaEl.value.split(' ').length;
+  // Asignar valores
+  if (lengthOfCharacters === 0) {
+    wordNumberEl.textContent = 0;
+  } else {
+    const wordCount = textareaEl.value.split(" ").length;
     wordNumberEl.textContent = wordCount;
+  }
 
-    const characterCount = textareaEl.value.length;
-    characterNumberEl.textContent = characterCount;
+  const characterCount = textareaEl.value.length;
+  characterNumberEl.textContent = characterCount;
 
-    const twitterTextCount = 280 - lengthOfCharacters;
-    twitterNumberEl.textContent = twitterTextCount;
+  const twitterTextCount = 280 - lengthOfCharacters;
+  twitterNumberEl.textContent = twitterTextCount;
 
-    const facebookTextcount = 2200 - lengthOfCharacters;
-    facebookNumberEl.textContent = facebookTextcount;
+  const facebookTextcount = 2200 - lengthOfCharacters;
+  facebookNumberEl.textContent = facebookTextcount;
 
-    if (twitterTextCount < 0) {
-        twitterNumberEl.style.color = 'red';
-    }
+  if (twitterTextCount < 0) {
+    twitterNumberEl.style.color = "red";
+  }
 
-    if (facebookTextcount < 0) {
-        facebookNumberEl.style.color = 'red';
-    }
-
-}) 
+  if (facebookTextcount < 0) {
+    facebookNumberEl.style.color = "red";
+  }
+});
